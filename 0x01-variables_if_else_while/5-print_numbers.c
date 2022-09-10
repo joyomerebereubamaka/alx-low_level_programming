aaf3e36427ed5b6baacb01e59684c9732cb98a3b
Your Name <you@example.com>
2022-09-10 09:36:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+
+	return (0);
+}
