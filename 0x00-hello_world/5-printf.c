a2a118afb8fbda7086b6ba92f12f03c3da1496ca
Your Name <you@example.com>
2022-09-10 03:18:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
-
 /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Succes)
-  *
-  */
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
 int main(void)
-
 {
-         printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n"); 
+	return (0);
 }
