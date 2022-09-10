b5bf5f0c4ec6a306ea460c1e21ea7293beed7792
Your Name <you@example.com>
2022-09-10 08:57:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ *  main- Entry point
+ *
+ *  Return: Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	for (; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
