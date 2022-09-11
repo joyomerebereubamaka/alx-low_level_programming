6c37f62c5c8af631a89377800677f38d933787b8
Your Name <you@example.com>
2022-09-11 03:18:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
