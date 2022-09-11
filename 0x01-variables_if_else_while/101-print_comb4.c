f8a898a691144a7140df6924c1c177abb8ebbacd
Your Name <you@example.com>
2022-09-11 04:16:11 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Sucess)
+ *
+ */
+int main(void)
+{
+	int n = 0;
+	int m;
+	int o;
+
+	for (; n < 8; n++)
+	{
+		for (m = n + 1; m < 9; m++)
+		{
+			for (o = m + 1; o < 10; o++)
+			{
+				putchar((n % 10) + '0');
+				putchar((m % 10) + '0');
+				putchar((o % 10) + '0');
+				if (n == 7 && m  == 8 && o == 9)
+				{
+					continue;
+				}
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+return (0);
+}
